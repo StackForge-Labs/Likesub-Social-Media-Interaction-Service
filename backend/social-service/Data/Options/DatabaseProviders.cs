75e3e7f02747024d42db17e06e853bc173d5c1c0
Le Minh Tri <leminhtri260903@gmail.com>
2026-02-07 03:26:14 +0700

    remove microserve model

--- backend/social-service/Data/Options/DatabaseProviders.cs
+++
@@ -1,8 +0,0 @@
-namespace social_service.Data.Options;
-
-public static class DatabaseProviders
-{
-    public const string MySql = "mysql";
-    public const string PostgreSql = "postgresql";
-    public const string Postgres = "postgres";
-}
